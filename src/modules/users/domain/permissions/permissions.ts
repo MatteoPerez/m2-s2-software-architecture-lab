1ebf71348bfa54a3b74db4a7c6b51db55e0dcf4a
MatteoPerez <matteo.perez@student.junia.com>
2026-03-11 23:06:51 +0100

    Modif permissions.ts pour ajouter les tags

--- src/modules/users/domain/permissions/permissions.ts
+++ src/modules/users/domain/permissions/permissions.ts
@@ -1,10 +1,13 @@
 import { UserRole } from '../entities/user.entity';
 import { PostPermissions } from './post-permissions';
+import { TagPermissions } from './tag-permissions';
 
 export class Permissions {
   public readonly posts: PostPermissions;
+  public readonly tags: TagPermissions;
 
   constructor(userId: string, role: UserRole) {
     this.posts = new PostPermissions(userId, role);
+    this.tags = new TagPermissions(role);
   }
 }
