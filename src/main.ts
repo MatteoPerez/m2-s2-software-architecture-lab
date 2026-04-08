e969bce1dede7801ed8a2002a3eec4dc7e48bc20
Matteo Perez <matteo.perez@altitudeinfra.fr>
2026-04-08 17:48:54 +0200

    Ajout setup de la documentation Swagger dans main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DomainExceptionFilter } from './modules/shared/errors/infrastructure/filters/domain-exception.filter';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalFilters(new DomainExceptionFilter());
@@ -11,6 +14,18 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') ?? 3000;
 
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Software Architecture Lab API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(port);
+
+  logger.log(`Swagger documentation accessible at http://localhost:${port}/api`);
 }
 bootstrap();
